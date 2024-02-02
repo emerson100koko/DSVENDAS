a8bfe61e642649e1d8f0317a943e716281c1944d
Emerson Gomes Nunes <emerson100loko@hotmail.com>
2024-02-01 22:51:37 -0300

    basic stats components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/emerson100koko" target="_blank" rel="noreferrer">Emerson Gomes</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/emerson.gomesnunes" target="_blank" rel="noreferrer">@emerson.gomesnunes</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
