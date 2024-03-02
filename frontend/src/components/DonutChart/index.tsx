2fb04ab40472485db4d6b2aaed26fded5aec7a65
Emerson Gomes Nunes <emerson100loko@hotmail.com>
2024-03-02 11:24:34 -0300

    BarChart integratioj

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -11,7 +11,7 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
 
     useEffect(() => {
         axios.get(`${BASE_URL} /sales/amount-by-seller`)
@@ -20,18 +20,13 @@ const DonutChart = () => {
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
 
-                setChartData({ labels: myLabels, series: mySeries });
+                setChartData({ labels: myLabels, series: mySeries});
             });
     }, []);
 
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-
     const options = {
         legend: {
-            show: true
+            show: true 
         }
     }
 
--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -4,32 +4,33 @@ import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
-export default function Dashboard() {
-  return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+const Dashboard = () => {
+    return (
+        <>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-            <BarChart />
-          </div>
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secundary">Taxa de Sucesso (%)</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secundary">Todas Vendas</h5>
+                        <DonutChart />
+                    </div>
+                </div>
 
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as vendas</h5>
-            <DonutChart />
-          </div>
-        </div>
+                <div className="py-3">
+                    <h2 className="text-primary">Todas Vendas </h2>
+                </div>
 
-        <div className="py-3">
-          <h2 className="text-primary">Todas as vendas</h2>
-        </div>
-
-        <DataTable />
-      </div>
-      <Footer />
-    </>
-  );
+                <DataTable />
+            </div>
+            <Footer />
+        </>
+    );
 }
+
+export default Dashboard;
